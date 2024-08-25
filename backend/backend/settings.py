40c99cea7a765b38bcf97225baad1dc000f4fbd3
Mnae Vardazaryan <manevardazaryan@gmail.com>
2024-08-25 01:39:58 -0800

    Edit settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,10 +53,11 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "static/"
+STATIC_ROOT = "static"
+STATICFILES_DIRS = [BASE_DIR / "static_files/static"]
+MEDIA_ROOT = "media"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 ROOT_URLCONF = "backend.backend.urls"
 
 TEMPLATES = [
@@ -126,8 +127,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
