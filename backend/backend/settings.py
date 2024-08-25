cdb9ac0c0c973ad2a687521c75b01355b5bc0f81
Mnae Vardazaryan <manevardazaryan@gmail.com>
2024-08-25 04:46:43 -0800

    Change home app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    "home.apps.HomeConfig",
+    "home"
 ]
 
 MIDDLEWARE = [
--- backend/home/views.py
+++ backend/home/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to my Django app!")
+def index(request):
+    return HttpResponse("Hello, world. You're at the home index.")
