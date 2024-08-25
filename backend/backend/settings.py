d6d33e7ab714913a00b65241521edf98832a1982
Mnae Vardazaryan <manevardazaryan@gmail.com>
2024-08-25 01:32:14 -0800

    Edit settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-ROOT_URLCONF = "backend.urls"
+ROOT_URLCONF = "backend.backend.urls"
 
 TEMPLATES = [
     {
